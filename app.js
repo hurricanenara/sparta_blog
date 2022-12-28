a74f85472dd6b2c2bf3f51633dc3493363171039
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:21:58 -0500

    create posts router and connect add to server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const postRouter = require("./routes/posts");
+
 const app = express();
 
 mongoose.connect("mongodb://localhost/spart_posts");
@@ -8,5 +10,7 @@ const db = mongoose.connection;
 
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("db connected!"));
+
+app.use("/posts", postRouter);
 
 app.listen(3000, () => console.log("server started"));
---
+++ routes/posts.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("./schema/post");
+
+module.exports = router;
