9e4f132ba448b340c37bfd44c97712596713c7a9
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:34:02 -0500

    create commentSchema and export

---
+++ routes/schema/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Types;
+
+const commentSchema = mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  postId: {
+    type: ObjectId,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
