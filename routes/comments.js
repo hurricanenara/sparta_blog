655a851df46adc1b95edc80ef7bcf223c6dc92c6
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:34:38 -0500

    add GET /comments api

--- routes/comments.js
+++ routes/comments.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const Comment = require("./schema/comment");
+
+router.get("/", async (_, res) => {
+  try {
+    const comments = await Comment.find().select(["-password"]);
+    res.json({ data: comments });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
