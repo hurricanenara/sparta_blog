ce3491334b3d20f47e4de03a9b7ba2f96b877094
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:23:02 -0500

    add GET /posts api

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Post = require("./schema/post");
 
+router.get("/", async (_, res) => {
+  try {
+    const posts = await Post.find().select(["-password"]); // [{}, {}, {}]
+    res.json({ data: posts });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
