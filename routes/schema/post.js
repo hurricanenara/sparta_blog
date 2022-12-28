9d097e92f426662d41ee8694b32162241a99592f
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:31:04 -0500

    create postSchema and export

---
+++ routes/schema/post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
