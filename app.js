2d62578527376d62333456d674be50f54dc72566
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:27:02 -0500

    configure express to handle json req

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("db connected!"));
 
+app.use(express.json());
 app.use("/posts", postRouter);
 
 app.listen(3000, () => console.log("server started"));
