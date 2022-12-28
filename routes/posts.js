2835d5db0439de1f6dac07e6052e85318b6505d6
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:28:45 -0500

    add PATCH /posts/:id api

--- routes/posts.js
+++ routes/posts.js
@@ -41,4 +41,45 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!req.body || !id) {
+    return res.json({ message: "데이터 형식이 올바르지 않습니다." });
+  }
+
+  const { user, title, content, password } = req.body;
+
+  const post = await Post.findById(id); // null
+
+  if (!post) {
+    return res.json({ message: "게시글 조회에 실패하였습니다." });
+  }
+
+  const isPasswordCorrect = post.password === password;
+
+  if (isPasswordCorrect) {
+    if (user) {
+      post.user = user;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    try {
+      const updatedPost = await post.save();
+      res.json({ data: updatedPost });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(401).json({ message: "비밀번호가 틀렸습니다." });
+  }
+});
+
 module.exports = router;
