14c46639ef0755a8f224e3d3af59a2aea1327f3d
hurricanenara <naralee0124@gmail.com>
2022-12-28 08:20:04 -0500

    connect mongoDB with mongoose

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/spart_posts");
+const db = mongoose.connection;
+
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("db connected!"));
 
 app.listen(3000, () => console.log("server started"));
